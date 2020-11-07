f739f9960cb8b6120565cffe62ee74f6bf11123a
Zbigniew Mandziejewicz <shaxbee@gmail.com>
2020-11-07 21:16:53 +0400

    feat: add run-skaffold and dev-skaffold targets

--- makefiles/skaffold.mk
+++ makefiles/skaffold.mk
@@ -12,25 +12,29 @@ $(SKAFFOLD):
 	curl -sSfL https://storage.googleapis.com/skaffold/releases/v$(SKAFFOLD_VERSION)/skaffold-$(OS)-amd64 -o $(SKAFFOLD)
 	chmod u+x $(SKAFFOLD)
 
-.PHONY: clean-skaffold build-skaffold deploy-skaffold
+.PHONY: clean-skaffold build-skaffold deploy-skaffold dev-skaffold
 
-clean: clean-skaffold
-
-clean-bin: clean-skaffold
-
-clean-skaffold: $(SKAFFOLD)
+clean-skaffold: $(SKAFFOLD) ## Clean Skaffold stack
 	$(info $(_bullet) Cleaning <skaffold>)
-	! kubectl config current-context 2>/dev/null || \
+	! kubectl config current-context &>/dev/null || \
 	$(SKAFFOLD) delete
 
-build-skaffold: $(SKAFFOLD) ## Build artifacts with skaffold
+build-skaffold: $(SKAFFOLD) ## Build artifacts with Skaffold
 	$(info $(_bullet) Building artifacts with <skaffold>)
 	$(SKAFFOLD) build
 
 deploy: deploy-skaffold
 
-deploy-skaffold: $(KUBECTL) $(SKAFFOLD) build-skaffold ## Deploy artifacts with skaffold
+deploy-skaffold: $(SKAFFOLD) build-skaffold ## Deploy artifacts with Skaffold
 	$(info $(_bullet) Deploying with <skaffold>)
 	$(SKAFFOLD) build -q | $(SKAFFOLD) deploy --force --build-artifacts -
+
+run-skaffold: $(SKAFFOLD)
+	$(info $(_bullet) Run stack with <skaffold>)	
+	$(SKAFFOLD) run --force --port-forward
+
+dev-skaffold: $(SKAFFOLD) ## Run stack in development mode with Skaffold
+	$(info $(_bullet) Run stack in development mode with <skaffold>)
+	$(SKAFFOLD) dev --force --port-forward
 
 endif
